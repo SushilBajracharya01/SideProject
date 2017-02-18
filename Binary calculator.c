dea0c3a9a24cb238d8178c6e5dd2f8e5e86813a3
Sushil Bajracharya <Bajracharya.sushil01@gmail.com>
2017-02-18 09:53:14 +0545

    Update Binary calculator.c

--- Binary calculator.c
+++ Binary calculator.c
@@ -3,17 +3,16 @@
 #include <stdio.h>
 #include <math.h>
 
-int len(int N);
 int decon(int N);
-int bicon(int,int);
-int check(int,int);
+int bicon(int);
+int check(int);
 int add(int n1,int n2);
 int sub(int n1,int n2);
 
 int mod,i,div;
 
 int main(){
-	int len1,ch,chk,len2,num1,check1,check2,num2,bicon1,bicon2;
+	int ch,chk,num1,check1,check2,num2,bicon1,bicon2;
 	printf("This is a program to add or subtract 2 binary numbers\n\t To add press 1, To subtract press 2 :");
 	scanf("%d",&chk);
 	printf("\n****Enter two numbers****\n"); 
@@ -22,14 +21,9 @@ int main(){
 	printf("Enter binary number 2 :");
 	scanf("%d",&num2);
 	
-	
-	//functions to calculate length of number.
-	len1=len(num1);
-	len2=len(num2);
-	
 	//Functions to check whether the numbers are in binary.
-	check1=check(num1,len1);
-	check2=check(num2,len2);
+	check1=check(num1);
+	check2=check(num2);
 	
 	if(0==(check1 && check2)){
 		printf("\n\t\tINVALID INPUT\n");
@@ -42,8 +36,8 @@ int main(){
 	
 	else{
 	//a function to convert binary to decimal number.
-	bicon1 = bicon(num1,len1);
-	bicon2 = bicon(num2,len2);
+	bicon1 = bicon(num1);
+	bicon2 = bicon(num2);
 	
 	//printf("%d\n",bicon1);
 	//printf("%d\n",bicon2);
@@ -66,20 +60,10 @@ int main(){
 }
 }
 
-//a function to calculate length of number.
-int len(int N){
-		int length = 1;
-		while(N > 9){
-			length++;
-			N = N / 10;
-		}
-		return length;
-}
-
 //a Function to check whether the numbers are in binary.
-int check(int N,int len){
+int check(int N){
 	mod=0;
-	for(i=0;i<len;i++){
+	while(N>0){
 		mod=N%10;
 		N=N/10;
 		if(mod>1){
@@ -90,9 +74,9 @@ int check(int N,int len){
 }	
 
 //a function to convert binary to decimal number.
-int bicon(int n,int len){
+int bicon(int n){
 	int mod=0,p=0,con=0;
-	for(i=0;i<len;i++){
+	while(n>0){
 		mod=n%10;
 		n=n/10;
 		p=pow(2,i);
